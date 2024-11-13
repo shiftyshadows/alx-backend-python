284924545a6f2947279e030e28b93e1b3d147f50
ShiftyShadows <purple.quasar@proton.me>
2024-11-13 21:02:48 +0300

    12. Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 """ This module defines the function: zoom_array. """
-from typing import Tuple, List, Union
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Return a list with each element in lst repeated factor times.
 
     Args:
-        lst (Tuple[int, ...]): A tuple of integers.
+        lst (Tuple): A tuple of elements.
         factor (int): The repeat factor for each element in the tuple.
 
     Returns:
-        List[int]: A list where each element is repeated factor times.
+        List: A list where each element is repeated factor times.
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for _ in range(factor)
     ]
     return zoomed_in
 
 
-array = (12, 72, 91)  # Changed to a tuple to match function annotation
+array = (12, 72, 91)  # Converted to a tuple to match function annotation
 
 zoom_2x = zoom_array(array)  # Default factor of 2
 zoom_3x = zoom_array(array, 3)  # Pass an integer for factor
