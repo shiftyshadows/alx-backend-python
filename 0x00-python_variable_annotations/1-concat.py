7ef20a1acad0b553b0fee60dfd8e6329f8f56d3a
ShiftyShadows <purple.quasar@proton.me>
2024-11-13 18:43:34 +0300

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+   This module defines the function concat.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string of str1 and str2.
+    """
+    return str1 + str2
