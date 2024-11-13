c205a2dde994950bc19a54c8307163ea5c7dc668
ShiftyShadows <purple.quasar@proton.me>
2024-11-13 18:40:37 +0300

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This Module defines the function add"""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two float numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
