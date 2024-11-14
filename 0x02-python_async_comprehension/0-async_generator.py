398c593a05ce332f6e23b8e6ef2f87d771319381
ShiftyShadows <purple.quasar@proton.me>
2024-11-14 13:27:05 +0300

    1. Async Comprehensions

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    This module defines the async
+    coroutine: async_generator.
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Coroutine that yields a random number between 0 and 10,
+    with a 1-second delay in each of its 10 iterations.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+   This module defines the asychronous
+   coroutine: async_comprehension.
+"""
+import importlib
+import asyncio
+
+# Import async_generator from the previous task using importlib
+async_generator_module = importlib.import_module('0-async_generator')
+async_generator = async_generator_module.async_generator
+
+
+async def async_comprehension():
+    """
+    Coroutine that collects 10 random numbers from async_generator
+    using an async comprehension, and returns them as a list.
+    """
+    return [number async for number in async_generator()]
